fee8ae59a43a610fac3caba22cfa14ffb19a0c69
Freitag <nepiz@i.ua>
2019-04-08 11:34:29 +0400

    fp done

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -1,9 +1,6 @@
 module Exercise
   module Fp
     class << self
-      # Обратиться к параметрам фильма можно так:
-      # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
-      # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
         country = array.select { |film| film['country'].to_s.split(',').length >= 2 }
         rating = country.select { |film| film['rating_kinopoisk'].to_f >= 2 }
--- test/exercise/fp/test.rb
+++ test/exercise/fp/test.rb
@@ -1,18 +1,14 @@
 require 'csv'
 require './test/test_helper.rb'
 require_relative './solution.rb'
-
-# Оба задания нужно решить используя map и reduce
 class Exercise::FpTest < Minitest::Test
-  # Посчитать средний рейтинг фильмов по версии кинопоиска у которых две или больше стран
-  # Фильмы у которых рейтиг не задан или равен 0 не учитывать в расчете среднего.
+
   def test_rating
     array = CSV.readlines('./test/fixtures/films.csv', headers: true)
     result = Exercise::Fp.rating(array)
     assert result == 6.809410385259628
   end
 
-  # Посчитать количесвто букв 'и' в названиях всех фильмов с рейтингом кинопоиска больше или равным заданному значению
   def test_chars_count
     array = CSV.readlines('./test/fixtures/films.csv', headers: true)
 
